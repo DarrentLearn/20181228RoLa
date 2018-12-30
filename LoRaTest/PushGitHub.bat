2efcfa6968d98202d56f79748e154d5a504590ff
Darrent Chen <44484851+DarrentLearn@users.noreply.github.com>
2018-12-30 22:48:46 +0800

    Delete PushGitHub.bat

--- LoRaTest/PushGitHub.bat
+++
@@ -1,6 +0,0 @@
-echo "# 20181228RoLa" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/DarrentLearn/20181228RoLa.git
-git push -u origin master
